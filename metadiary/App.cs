31d28acfaf28ee3c8a7f523989d89f1d33f7a50e
Manoj Baishya <manojbaishya@users.noreply.github.com>
2024-05-18 17:07:02 +0530

    2024-05-18T05:07:00.00+05:30 Created 'metadiary' project

---
+++ metadiary/App.cs
@@ -0,0 +1,8 @@
+﻿using CommandDotNet;
+
+namespace Metadiary;
+
+public static class App
+{
+    private static int Main(string[] args) => new AppRunner<MetadataReader>().UseDefaultMiddleware().Run(args);
+}
---
+++ metadiary/MetadataReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MetadataExtractor;
+
+namespace Metadiary;
+public class MetadataReader(string filename)
+{
+    private readonly IReadOnlyList<Directory> directories = ImageMetadataReader.ReadMetadata(filename);
+
+    public void GetMetadata()
+    {
+        foreach (string? tag in directories.SelectMany(d => d.Tags, (_, tag) => tag.Name))
+        {
+            Console.WriteLine(tag);
+        }
+    }
+}
