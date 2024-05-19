0c6d8c68fd8d0c272d1953108d5340e49b97f11e
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2024-05-19 11:55:00 +0530

    Add dotnet pipeline with xUnit tests (#5)
    * Add dotnet pipeline with xUnit tests
    
    * restore docfx pipeline
    
    ---------
    
    Co-authored-by: Manoj Baishya <manojbaishya@users.noreply.github.com>

---
+++ Metadiary/Metadata/MediaType.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.SmartEnum;
+
+namespace Metadiary;
+
+public sealed class MediaType : SmartEnum<MediaType>
+{
+    public static readonly MediaType Image = new(nameof(Image), 1);
+    public static readonly MediaType Audio = new(nameof(Audio), 2);
+    public static readonly MediaType Video = new(nameof(Video), 3);
+
+    private MediaType(string name, int value) : base(name, value) { }
+}
