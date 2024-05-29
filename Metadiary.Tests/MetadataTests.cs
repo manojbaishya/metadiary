323167228a04c30e470cb017339a011a24af95e5
Manoj Baishya <manojbaishya@users.noreply.github.com>
2024-05-29 10:29:32 +0530

    changes

--- Metadiary.Tests/MetadataTests.cs
+++ Metadiary.Tests/MetadataTests.cs
@@ -6,20 +6,20 @@ namespace Metadiary.Tests;
 public class MetadataTests
 {
     [Theory]
-    [InlineData("Image")]
-    [InlineData("Audio")]
-    [InlineData("Video")]
+    [InlineData("image")]
+    [InlineData("audio")]
+    [InlineData("video")]
     [InlineData("Invalid")]
     public void TestGetMediaType(string filetype)
     {
         try
         {
-            MediaType mediaType = MetadataReader.GetMediaType(filetype);
+            MediaType mediaType = MetadataReader.GetMediaType(filetype.ToLower());
             mediaType.Should().NotBeNull();
             mediaType.ToString().Should().Be(filetype);
         } catch (SmartEnumNotFoundException enumNotFoundException)
         {
-            enumNotFoundException.Message.Should().Be(@$"No MediaType with Name ""{filetype}"" found.");
+            enumNotFoundException.Message.Should().Be(@$"No MediaType with Name ""{filetype.ToLower()}"" found.");
         }
     }
 }
